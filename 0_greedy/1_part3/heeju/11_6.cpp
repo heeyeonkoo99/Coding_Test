345e1d19d4ac5e3932845a99793637c8ec783bb4
ahjheeju129 <heeju1112@naver.com>
2021-08-06 16:46:22 +0900

    11_6 수정

--- 0_greedy/1_part3/heeju/11_6.cpp
+++ 0_greedy/1_part3/heeju/11_6.cpp
@@ -1,27 +1,34 @@
 #include<iostream>
 #include <string>
 #include <vector>
+#include<algorithm>
 
 using namespace std;
 
 int solution(vector<int> food_times, long long k) {
     int answer = 0;
-
     int size = food_times.size();
-    for (int i = 0; i <= k ; i++) {
-        int idx = i % size;
-        int cnt = 0;
-        while(food_times[idx]==0){
-            if (cnt == 3)
-                return -1;
-            idx = (++idx) % size;
-            cnt++;
-        }
-        food_times[idx]--;
-        if (i == k)
-            answer = idx;
-    }
+    int foodNum = 0;
 
+    for (int i = 0; i < k; i++) {
+        foodNum = (i + 1) % size; 
+        int cnt = 0;
+        while (1) {
+            if (food_times[foodNum - 1] != 0) {
+                food_times[foodNum - 1]--;
+                break;
+            }
+            else { //해당 번호 음식이 남아있지 않다면 -->다음번호로 넘어가야함
+                foodNum++;
+                cnt++;
+                if (foodNum > size )
+                    foodNum = 1;
+                if (cnt == size)
+                    return -1;
+            }
+        }
+    }
+    answer = (foodNum + 1) % size;
     return answer;
 }
 
