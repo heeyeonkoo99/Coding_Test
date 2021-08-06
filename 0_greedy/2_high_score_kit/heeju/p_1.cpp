3f7f89fb827f7e95fc1e009102640fc7cd587eaa
ahjheeju129 <heeju1112@naver.com>
2021-08-06 19:54:51 +0900

    .

---
+++ 0_greedy/2_high_score_kit/heeju/p_1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int solution(int n, vector<int>lost, vector<int> reserve) {
+	int result;
+	vector<int> std(n, 0);
+
+	sort(reserve.begin(), reserve.end());
+	for (int i = 0; i < reserve.size(); i++) {
+		int standard = reserve[i];
+		vector<int>::iterator front = find(lost.begin(), lost.end(), standard-1);
+		vector<int>::iterator self = find(lost.begin(), lost.end(), standard);
+		vector<int>::iterator back = find(lost.begin(), lost.end(), standard + 1);
+		if (self != lost.end()) { //내체육복 없음
+			lost.erase(self);
+			continue;
+		}
+		if (front != lost.end()) { //앞에 친구 체육복 없음.
+			lost.erase(front); //앞친구한테 빌려줌
+			continue;
+		}
+		if (back != lost.end()){
+				lost.erase(back);
+		}
+	}
+
+	result = n - lost.size();
+	return result;
+}
+
+int main() {
+	int n = 5;
+	int aLost[] = {2,4};
+	int aReserve[] = { 1,3,5 };
+	int result;
+	vector<int>lost(aLost, aLost + sizeof(aLost) / sizeof(aLost[0]));
+	vector<int> reserve(aReserve, aReserve + sizeof(aReserve) / sizeof(aReserve[0]));
+	result = solution(n, lost, reserve);
+	cout << result << endl;
+}
